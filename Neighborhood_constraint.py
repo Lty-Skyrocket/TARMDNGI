482feae7bccce2f14e43bc40c1425e2933a0fdad
Lty-Skyrocket <2070863503@qq.com>
2024-10-11 17:32:54 +0800

    Add files via upload

---
+++ Neighborhood_constraint.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+
+#计算药物和疾病的邻居相似性
+
+# 行归一化(邻居归一化）
+def row_normalization(S, k):
+    #计算邻居集合N
+    N = {}
+    for i in range(S.shape[0]):
+        neighbors = np.argsort(S[i])[::-1][:k]  # 获取相似性最高的k个邻居的索引
+        N[i] = list(neighbors)
+    #计算行归一化
+    result = np.zeros(S.shape)
+    for i in range(S.shape[0]):
+        num = 0
+        denominator=np.sum(S[i, N[i]]) # 分母计算的是邻居的和
+
+
+        for j in range(S.shape[1]):
+            if j in N[i]:
+                if denominator != 0:
+                    num = num + 1
+
+                    result[i, j] =  S[i, j] / denominator
+                else:
+                    result[i, j] = 0
+            else:
+                result[i, j] = 0
+    return result
+
